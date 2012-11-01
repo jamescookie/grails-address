20b734c42bee58ff730848b4475e2a8f9cf4812b
James Cook <james.cookie@gmail.com>
2012-11-01 08:38:44 +0000

    Added new project for testing 1.3.7 compatibility

---
+++ src/groovy/org/grails/plugins/address/GrailsVersionUtils.groovy
@@ -0,0 +1,11 @@
+package org.grails.plugins.address
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: James
+ * Date: 16/10/12
+ * Time: 13:19
+ * To change this template use File | Settings | File Templates.
+ */
+class GrailsVersionUtils {
+}
